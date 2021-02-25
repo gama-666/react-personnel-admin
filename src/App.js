cb245d2d1bb6db0b4c28f8152482e40e4df0d003
cyd <482264159@qq.com>
2021-02-25 17:00:02 +0800

    项目初始化-node-sass、sass全局变量配置、重置浏览器样式、ant design UI按需加载配置

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import { HashRouter, Switch, Route } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './styles/main.scss';
+import Home from './views/Home';
+import About from './views/About';
+import News from './views/News';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <Fragment>
+        <HashRouter>
+          <Switch>
+            <Route component={Home} exact path="/" />
+            <Route component={About} path="/about" />
+            <Route component={News} path="/news" />
+          </Switch>
+        </HashRouter>
+      </Fragment>
+    )
+  }
 }
+
 
 export default App;
